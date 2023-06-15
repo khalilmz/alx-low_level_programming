47262e9c126337ba1395f3c6c28a04fd6395e038
khalil <maaizikhalil@gmail.com>
2023-06-15 15:54:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+include < stdio.h >
+
+/**
+ * main - Entry point
+ * Hello, puts
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	"Programming is like building a multilingual puzzle
+		return (0);
+}
