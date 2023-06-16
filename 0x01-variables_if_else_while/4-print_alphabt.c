71ade8c41b123c6d929668991e69cd47b953eaae
khalil <maaizikhalil@gmail.com>
2023-06-16 16:12:48 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print all the letters in lowercase except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char word;
+
+	for (word = 'a'; word <= 'z'; word++)
+	{
+	if (word != 'q' && word != 'e')
+		{
+			putchar(word);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
