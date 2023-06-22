8cb1fd06f0de2e6409d4e574ca90ba9da6111475
khalil <maaizikhalil@gmail.com>
2023-06-22 13:53:23 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_most_numbers - prints from 0 to 9 without 2 and 4
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+	if (i != 2 || i != 4)
+	{
+	_putchar(i);
+	}
+	}
+	_putchar('\n');
+}
