b3fa48328adc11c9cc67c3e139027c6110adf2ee
khalil <maaizikhalil@gmail.com>
2023-07-17 15:15:03 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,7 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H
-#define _3_FUNCTION_LIKE_MACRO_H
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(X) ((X) < 0 ? (-X) : (X))
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
+
